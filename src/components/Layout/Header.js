56eb792f95801486176f9658349a1c08cf8839d2
barelimelech <barelimelech01@gmail.com>
2023-02-28 16:39:06 +0200

    Cart styling. Added Meals list and mealItem.

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
 import mealsImg from '../../assets/meals3.png';
-import classes from './Header.module.css'
+import classes from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
 
 const Header = (props) => {
   return (
     <React.Fragment>
         <header className={classes.header}>
             <h1>FoodOrderApp</h1>
-            <button>Cart</button>
+            <HeaderCartButton/>
         </header>
         <div className={classes['main-image']}>
             <img src={mealsImg} alt='A table full of delicious food!'/>

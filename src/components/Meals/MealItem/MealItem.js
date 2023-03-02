c2ca143f7306eeae9ac1d5f69e962d4436a46175
barelimelech <barelimelech01@gmail.com>
2023-03-02 11:18:26 +0200

    Added modal to Cart. Added meal item.

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import MealItemForm from './MealItemForm';
 import classes from './MealItem.module.css';
 
 const MealItem = (props) => {
@@ -9,7 +9,7 @@ const MealItem = (props) => {
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
         </div>
-        <div></div>
+        <div><MealItemForm id={props.id} /></div>
     </li>
   )
 };
